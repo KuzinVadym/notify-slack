09d4e4af1f25b6de2e19d93c2266048043b52371
Vadym Kuzin <8728056+KuzinVadym@users.noreply.github.com>
2024-02-07 21:02:29 +0100

    Create action.yml

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello World!');
+import { getInput } from '@actions/core';
+import * as github from '@actions/github';
+
+export async function run() {
+    const {
+        eventName,
+        sha,
+        ref,
+        actor,
+        payload,
+      } = github.context;
+
+    const name = getInput('name');
+
+    const branch = ref.slice(11);
+
+    console.log('branch');
+    console.log(branch);
+    console.log('ref');
+    console.log(ref);
+    console.log('eventName');
+    console.log(eventName);
+    console.log('payload');
+    console.log(payload);
+    console.log('actor');
+    console.log(actor);
+
+    console.log(`Hello ${name}`);
+}
+
+run();
