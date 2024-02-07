5d6d0ca7983012ea26f72235cf6a19db78fa2325
KuzinVadym <vadym.k2.kuzin@gmail.com>
2024-02-07 23:29:23 +0100

    test custom action with pull request

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,53 @@
-import { getInput } from '@actions/core';
+import { getInput, setFailed } from '@actions/core';
 import * as github from '@actions/github';
+import { App } from '@slack/bolt';
+
+async function sendMessageToChannel(app: any, message: string) {
+    const temp = await app.client.chat.postMessage({
+        channel: 'webhooks',
+        text: message
+    })
+}
 
 export async function run() {
-    const {
-        eventName,
-        sha,
-        ref,
-        actor,
-        payload,
-      } = github.context;
+    try {
+        const {
+            eventName,
+            ref,
+            actor,
+            payload,
+          } = github.context;
+    
+        const slackSignInSecret = getInput('slackSignInSecret');
+    
+        if(!slackSignInSecret) {
+            throw new Error('Slack SignInSecret not provided')
+        }
 
-    const name = getInput('name');
-
-    const branch = ref.slice(11);
-
-    console.log('branch');
-    console.log(branch);
-    console.log('ref');
-    console.log(ref);
-    console.log('eventName');
-    console.log(eventName);
-    console.log('payload');
-    console.log(payload);
-    console.log('actor');
-    console.log(actor);
-
-    console.log(`Hello ${name}`);
+        const slackBotToken = getInput('slackBotToken');
+    
+        if(!slackBotToken) {
+            throw new Error('Slack BotToken not provided')
+        }
+    
+        let branch = ref.slice(11);
+    
+        if (payload.pull_request) {
+            branch = payload.pull_request.head.ref;
+        }
+    
+        const app = new App({
+            signingSecret: slackSignInSecret,
+            token: slackBotToken,
+        });
+    
+        const message = `User ${actor} add new ${eventName} from ${branch} branch to main`
+    
+        await sendMessageToChannel(app, message);
+    } catch (err) {
+        console.log(err);
+        setFailed(err as any);
+    }
 }
 
 run();
